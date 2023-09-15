831ed9542ace5ef580a4351ffa3c50e7a163b561
Jlat21 <jared.laturnus1@gmail.com>
2023-09-15 13:17:45 -0600

    Update StudentInfo.jsx
    swapped the anchor tag to a link tag

--- app/StudentInfo/StudentInfo.jsx
+++ app/StudentInfo/StudentInfo.jsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 const StudentInfo = () => {
     return (
         <div classname="studentinfo">
             <h1>Jared Laturnus</h1>
             <h2>CPRG 306 A</h2>
-            <a href="https://github.com/JLat21" target="_blank">GitHub</a>
+            <Link href="https://github.com/JLat21" target="_blank">GitHub</Link>
         </div>
     );
 }
