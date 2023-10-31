13a84e8f5b56f703875266b55849379c7660896c
Jlat21 <jared.laturnus1@gmail.com>
2023-10-30 21:49:07 -0600

    week 7 and week 8

---
+++ app/week8/page.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import StudentInfo from "../StudentInfo/StudentInfo";
+import NewItem from "../week4/new-item";
+export default function week4() {
+  return (
+    <div>
+      <h1 className="text-4xl m-5">My Shopping List</h1>
+      <StudentInfo />
+      <Link href="/">Home</Link>
+      <NewItem />
+    </div>
+
+  )
+  
+  
+}
